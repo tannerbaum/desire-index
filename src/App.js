3912eb286aea634678eacd6ecdfd2480cb326794
Tanner Hoisington <tanner@Tanners-MBP.fritz.box>
2019-07-09 22:25:07 +0200

    updates dependecy risks and changes export

--- src/App.js
+++ src/App.js
@@ -5,101 +5,95 @@ import logo from "./logo.png";
 import "./App.css";
 import Standings from "./Components/Standings/Standings";
 import EpisodeDetails from "./Components/EpisodeDetails/EpisodeDetails";
-import { timeLine } from "./data/indexTimeLine";
+import { index1 } from "./data/indexTimeLine";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            thisWeek: 0,
-            lastWeek: -1,
-            atBeginning: true,
-            atEnd: false
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      thisWeek: 0,
+      lastWeek: -1,
+      atBeginning: true,
+      atEnd: false
+    };
 
-        this.goWeekForward = this.goWeekForward.bind(this);
-        this.goWeekBackward = this.goWeekBackward.bind(this);
-    }
+    this.goWeekForward = this.goWeekForward.bind(this);
+    this.goWeekBackward = this.goWeekBackward.bind(this);
+  }
 
-    goWeekBackward() {
-        this.setState(prevState => {
-            const thisWeek = prevState.thisWeek - 1;
-            const lastWeek = prevState.lastWeek - 1;
+  goWeekBackward() {
+    this.setState(prevState => {
+      const thisWeek = prevState.thisWeek - 1;
+      const lastWeek = prevState.lastWeek - 1;
 
-            return {
-                thisWeek,
-                lastWeek,
-                atBeginning: thisWeek === 0 ? true : false,
-                atEnd: thisWeek + 1 === timeLine.length ? true : false
-            };
-        });
-    }
+      return {
+        thisWeek,
+        lastWeek,
+        atBeginning: thisWeek === 0 ? true : false,
+        atEnd: thisWeek + 1 === index1.length ? true : false
+      };
+    });
+  }
 
-    goWeekForward() {
-        this.setState(prevState => {
-            const thisWeek = prevState.thisWeek + 1;
-            const lastWeek = prevState.lastWeek + 1;
+  goWeekForward() {
+    this.setState(prevState => {
+      const thisWeek = prevState.thisWeek + 1;
+      const lastWeek = prevState.lastWeek + 1;
 
-            return {
-                thisWeek,
-                lastWeek,
-                atBeginning: thisWeek === 0 ? true : false,
-                atEnd: thisWeek + 1 === timeLine.length ? true : false
-            };
-        });
-    }
+      return {
+        thisWeek,
+        lastWeek,
+        atBeginning: thisWeek === 0 ? true : false,
+        atEnd: thisWeek + 1 === index1.length ? true : false
+      };
+    });
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Desire Index (RIP)</h1>
-                    <div className="buttons">
-                        <button
-                            onClick={this.goWeekBackward}
-                            disabled={this.state.atBeginning}
-                        >
-                            Prev
-                        </button>
-                        <button
-                            onClick={this.goWeekForward}
-                            disabled={this.state.atEnd}
-                        >
-                            Next
-                        </button>
-                    </div>
-                </header>
-                <section>
-                    <Standings
-                        gamesThisWeek={timeLine[this.state.thisWeek].games}
-                        gamesLastWeek={
-                            this.state.lastWeek >= 0
-                                ? timeLine[this.state.lastWeek].games
-                                : null
-                        }
-                    />
-                </section>
-                <div className="episode">
-                    <EpisodeDetails episode={timeLine[this.state.thisWeek]} />
-                    <div className="buttons">
-                        <button
-                            onClick={this.goWeekBackward}
-                            disabled={this.state.atBeginning}
-                        >
-                            Prev
-                        </button>
-                        <button
-                            onClick={this.goWeekForward}
-                            disabled={this.state.atEnd}
-                        >
-                            Next
-                        </button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Desire Index (RIP)</h1>
+          <div className="buttons">
+            <button
+              onClick={this.goWeekBackward}
+              disabled={this.state.atBeginning}
+            >
+              Prev
+            </button>
+            <button onClick={this.goWeekForward} disabled={this.state.atEnd}>
+              Next
+            </button>
+          </div>
+        </header>
+        <section>
+          <Standings
+            gamesThisWeek={index1[this.state.thisWeek].games}
+            gamesLastWeek={
+              this.state.lastWeek >= 0
+                ? index1[this.state.lastWeek].games
+                : null
+            }
+          />
+        </section>
+        <div className="episode">
+          <EpisodeDetails episode={index1[this.state.thisWeek]} />
+          <div className="buttons">
+            <button
+              onClick={this.goWeekBackward}
+              disabled={this.state.atBeginning}
+            >
+              Prev
+            </button>
+            <button onClick={this.goWeekForward} disabled={this.state.atEnd}>
+              Next
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
